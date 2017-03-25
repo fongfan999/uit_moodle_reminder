61b5de7f6cb98762eff41320d5726b0a0539c8ad
Phong Phan <fongfan999@gmail.com>
2017-03-25 09:37:48 +0700

    Using callback on cleaning events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   has_many :reminders, dependent: :delete_all
   has_many :users, through: :reminders
 
-  scope :clean, -> { where("date < ?", Time.zone.now).delete_all }
+  scope :clean, -> { where("date < ?", Time.zone.now).destroy_all }
 
   def self.find_by_link_or_initialize_by(params)
     where(params.slice(:link)).first_or_initialize(params)
