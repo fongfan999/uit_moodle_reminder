a827c2981a6f2a661d02f90b432c239e5234ba8c
Phong Phan <fongfan999@gmail.com>
2017-10-28 13:43:35 +0700

    Convert erb to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#new'
-  resources :users, only: [:new] do
+  resources :users, only: [:new, :create] do
     collection do
       post :subscribe
     end
