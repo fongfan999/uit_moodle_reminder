c18d68e86a3c3a5cce9653ec7225a8387e795966
Phong Phan <fongfan999@gmail.com>
2018-06-14 13:40:54 +0700

    Add healthcheck page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'users#new'
+
   resources :users, only: [:new] do
     collection do
       post :subscribe
     end
   end
 
-  get '/unsubscribe', to: "users#unsubscribe"
-  get '/unsubscribe_event', to: "users#unsubscribe_event"
+  get :unsubscribe, to: 'users#unsubscribe'
+  get :unsubscribe_event, to: 'users#unsubscribe_event'
+  get :healthcheck, to: 'pages#healthcheck'
 
   mount Facebook::Messenger::Server, at: 'bot'
 end
